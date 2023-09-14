40ba532ba80ec433b5f80994b0e481b3da73216b
ChoDongHyeon <whehdgus77@gmail.com>
2023-09-14 21:07:37 +0900

    Java_Study Ex04 반복문

---
+++ src/ex04/Ex04_08.java
@@ -0,0 +1,9 @@
+package ex04;
+
+public class Ex04_08 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 3; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
---
+++ src/ex04/Ex04_12.java
@@ -0,0 +1,11 @@
+package ex04;
+
+public class Ex04_12 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i-- != 0) {
+            System.out.println(i + " - I can do it.");
+        }
+    }
+}
